957a90abafcccef2af477aa97fabb02b22a0ae8b
Waldemar Kornewald <wkornewald>
2010-06-30 17:50:47 +0200

    pyjs filter compilation works. now need to check if everything runs

--- mediagenerator/filters/pyjs_filter.py
+++ mediagenerator/filters/pyjs_filter.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from mediagenerator.base import Filter
-from pyjs.translator import import_compiler, Translator
+from pyjs.translator import import_compiler, Translator, LIBRARY_PATH
 import os
 
 try:
@@ -8,90 +8,188 @@ try:
 except ImportError:
     from StringIO import StringIO
 
+PYJS_INIT_LIB_PATH = os.path.join(LIBRARY_PATH, 'builtin', 'public', '_pyjs.js')
+BUILTIN_PATH = os.path.join(LIBRARY_PATH, 'builtin')
+STDLIB_PATH = os.path.join(LIBRARY_PATH, 'lib')
+EXTRA_LIBS_PATH = os.path.join(os.path.dirname(__file__), 'pyjslibs')
+
+INIT_CODE = """
+var $wnd = window;
+var $doc = window.document;
+var $pyjs = new Object();
+$pyjs.global_namespace = this;
+$pyjs.__modules__ = {};
+$pyjs.modules_hash = {};
+$pyjs.loaded_modules = {};
+$pyjs.options = new Object();
+$pyjs.options.set_all = function (v) {
+    $pyjs.options.arg_ignore = v;
+    $pyjs.options.arg_count = v;
+    $pyjs.options.arg_is_instance = v;
+    $pyjs.options.arg_instance_type = v;
+    $pyjs.options.arg_kwarg_dup = v;
+    $pyjs.options.arg_kwarg_unexpected_keyword = v;
+    $pyjs.options.arg_kwarg_multiple_values = v;
+};
+$pyjs.options.set_all(true);
+%pyjs.options.dynamic_loading = false;
+$pyjs.trackstack = [];
+$pyjs.track = {module:'__main__', lineno: 1};
+$pyjs.trackstack.push($pyjs.track);
+$pyjs.__last_exception_stack__ = null;
+$pyjs.__last_exception__ = null;
+"""
+
 class Pyjs(Filter):
     takes_input = False
 
     def __init__(self, **kwargs):
-        self.config(kwargs, with_main_libs=False, main_module=None,
-                    debug=settings.DEBUG, path=(),
-                    only_dependencies=False)
+        self.config(kwargs, exclude_main_libs=False, main_module=None,
+                    debug=None, path=(), only_dependencies=True)
         if isinstance(self.path, basestring):
             self.path = (self.path,)
-        assert self.path and all(map(os.path.isdir, self.path)), (
-            'Please provide a valid root folder for Pyjs')
+        if self.only_dependencies:
+            self.path += (STDLIB_PATH, BUILTIN_PATH, EXTRA_LIBS_PATH)
         super(Pyjs, self).__init__(**kwargs)
         assert self.filetype == 'js', (
             'Pyjs only supports compilation to js. '
             'The parent filter expects "%s".' % self.filetype)
         self.input_filetype = 'pyjs'
 
-        self._visited = []
+        if self.only_dependencies:
+            assert self.main_module, \
+                'You must provide a main module in only_dependencies mode'
+
+        self._visited = set()
         self._compiled = {}
-        self._collected = set()
+        self._collected = {}
 
     def get_output(self, variation):
-        if self.with_main_libs:
-            yield self._compile_init()
-            yield self._compile_pyjslib()
+        self._collect_all_modules()
 
-        for name, content in self.get_input(variation):
-            yield self.compile(name, content)
+        if not self.exclude_main_libs:
+            yield self._compile_init()
+
+        if self.only_dependencies:
+            self._regenerate(dev_mode=False)
+            for name in sorted(self._visited):
+                yield self._compiled[name][1]
+        else:
+            for name in sorted(self._collected.keys()):
+                with open(self._collected[name], 'r') as fp:
+                    yield self._compile(name, fp.read(), dev_mode=False)[0]
 
         yield self._compile_main()
 
     def get_dev_output(self, name, variation):
-        if name == 'init.js':
+        self._collect_all_modules()
+
+        if name == '.init.js':
             return self._compile_init()
-        if name == 'pyjslib.js':
-            return self._compile_pyjslib()
-        elif name == 'main.js':
+        elif name == '.main.js':
             return self._compile_main()
 
-        content = super(Pyjs, self).get_dev_output(name, variation)
-        output, js_deps,  self.compile('test', content)
+        if self.only_dependencies:
+            self._regenerate(dev_mode=True)
+            return self._compiled[name][1]
+        else:
+            with open(self._collected[name], 'r') as fp:
+                return self._compile(name, fp.read(), dev_mode=True)[0]
 
     def get_dev_output_names(self, variation):
-        if self.with_main_libs:
-            yield 'init.js'
-            yield 'pyjslib.js'
+        self._collect_all_modules()
 
-        for name in super(Pyjs, self).get_dev_output_names(variation):
-            yield name
+        if not self.exclude_main_libs:
+            yield '.init.js'
 
-        if self.main_module is not None or self.with_main_libs:
-            yield 'main.js'
+        if self.only_dependencies:
+            self._regenerate(dev_mode=True)
+            for name in sorted(self._visited):
+                yield name
+        else:
+            for name in sorted(self._collected.keys()):
+                yield name
 
-    def compile(self, name, source):
+        if self.main_module is not None or not self.exclude_main_libs:
+            yield '.main.js'
+
+    def _regenerate(self, dev_mode=False):
+        # This function is only called in only_dependencies mode
+        if self._compiled:
+            for module_name, (mtime, content) in self._compiled.items():
+                if module_name not in self._collected or \
+                        os.path.getmtime(self._collected[module_name]) != mtime:
+                    # Just recompile everything
+                    # TODO: track dependencies and changes and recompile only
+                    # what's necessary
+                    self._visited = set()
+                    self._compiled = {}
+                    break
+
+            # No changes
+            return
+
+        modules = [self.main_module, 'pyjslib']
+        while True:
+            if not modules:
+                break
+
+            module_name = modules.pop()
+            path = self._collected[module_name]
+            with open(path, 'r') as fp:
+                source = fp.read()
+            mtime = os.path.getmtime(path)
+            content, py_deps, js_deps = self._compile(module_name, source, dev_mode=dev_mode)
+            self._visited.add(module_name)
+            self._compiled[module_name] = (mtime, content)
+
+            for name in py_deps:
+                if name not in self._collected:
+                    if '.' in name and name.rsplit('.', 1)[0] in self._collected:
+                        name = name.rsplit('.', 1)[0]
+                    else:
+                        raise ImportError('The pyjs module %s could not find '
+                            'the dependency %s' % (module_name, name))
+                if name not in self._visited:
+                    modules.append(name)
+
+    def _compile(self, name, source, dev_mode=False):
+        if self.debug is None:
+            debug = dev_mode
+        else:
+            debug = self.debug
         compiler = import_compiler(False)
         tree = compiler.parse(source)
         output = StringIO()
-        dependencies = Translator(compiler, name, name, source, tree, output,
-            debug=self.debug, source_tracking=self.debug,
-            line_tracking=self.debug, store_source=self.debug)
-        return output.getvalue(), dependencies[0], dependencies[1]
+        translator = Translator(compiler, name, name, source, tree, output,
+            debug=debug, source_tracking=debug, line_tracking=debug,
+            store_source=debug)
+        return output.getvalue(), translator.imported_modules, translator.imported_js
 
     def _compile_init(self):
-        pass
-
-    def _compile_pyjslib(self):
-        pass
+        with open(PYJS_INIT_LIB_PATH, 'r') as fp:
+            return INIT_CODE + fp.read()
 
     def _compile_main(self):
         content = ''
-        if self.with_main_libs:
-            content += '\n\npyjslib("pyjslib");'
+        if not self.exclude_main_libs:
+            content += '\n\n$pyjs.loaded_modules["pyjslib"]("pyjslib");'
+            content += '\n$pyjs.__modules__.pyjslib = $pyjs.loaded_modules["pyjslib"];'
         if self.main_module is not None:
-            content += '\n\n%s("%s");' % (2*(self.main_module,))
+            content += '\n\npyjslib.___import___("%s", null, "__main__");' % self.main_module
         return content
 
     def _collect_all_modules(self):
+        """Collect modules, so we can handle imports later"""
         for pkgroot in self.path:
             pkgroot = os.path.abspath(pkgroot)
             for root, dirs, files in os.walk(pkgroot):
                 if '__init__.py' in files:
                     files.remove('__init__.py')
-                    files.insert(0, '__init__.py')
-                else:
+                    # The root __init__.py is ignored
+                    if root != pkgroot:
+                        files.insert(0, '__init__.py')
+                elif root != pkgroot:
                     # Only add valid Python packages
                     dirs[:] = []
                     continue
@@ -101,17 +199,13 @@ class Pyjs(Filter):
                         continue
 
                     path = os.path.join(root, filename)
-                    module_path = filename[len(path)+len(os.sep):]
-
-                    if self.options.exclude and \
-                       re.match(self.options.exclude, module_path):
-                        continue
-
-                    if module_path.endswith('__init__.py'):
+                    module_path = path[len(pkgroot)+len(os.sep):]
+                    if os.path.basename(module_path) == '__init__.py':
                         module_name = os.path.dirname(module_path)
                     else:
-                        module_name = module_path[:-3] # cut ".py"
-                    module_name = module_name.replace(os.sep, ".")
+                        module_name = module_path[:-3]
+                    assert '.' not in module_name, \
+                        'Invalid module file name: %s' % module_path
+                    module_name = module_name.replace(os.sep, '.')
 
-                    print "%s (%s)" % (module_name, module_path)
-                    self.compile(module_path, module_name, self.options.working_dir)
+                    self._collected.setdefault(module_name, path)
---
+++ mediagenerator/filters/pyjslibs/dynamic.py
@@ -0,0 +1,2 @@
+# This is here because pyjslib.py imports the dynamic module.
+# However, we've turned off dynamic module loading, so this module is empty.
