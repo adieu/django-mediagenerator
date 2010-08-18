b8d59c12aa4fc45c442a314f42f7456337cef14a
Waldemar Kornewald <wkornewald>
2010-08-18 23:50:01 +0200

    updated setup script and added README

--- setup.py
+++ setup.py
@@ -1,21 +1,32 @@
 from setuptools import setup, find_packages
 
 DESCRIPTION = 'Extensible JavaScript/CSS combiner and compressor'
-
 LONG_DESCRIPTION = None
 try:
-    LONG_DESCRIPTION = open('README.rst').read()
+    LONG_DESCRIPTION = open('README').read()
 except:
     pass
 
 setup(name='django-mediagenerator',
-      version='0.5',
+      version='1.0',
       packages=find_packages(exclude=('tests', 'tests.*',
                                       'base_project', 'base_project.*')),
       package_data={'mediagenerator.filters': ['pyjslibs/*.py']},
       author='Waldemar Kornewald',
+      author_email='wkornewald@gmail.com',
       url='http://www.allbuttonspressed.com/projects/django-mediagenerator',
+      license='MIT',
       description=DESCRIPTION,
       long_description=LONG_DESCRIPTION,
       platforms=['any'],
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Web Environment',
+          'Framework :: Django',
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Libraries :: Application Frameworks',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
 )
