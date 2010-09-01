217499924d148f091b81d2c07de17fc331c52393
Waldemar Kornewald <wkornewald>
2010-09-01 10:54:09 +0200

    CopyFiles generator can now ignore files matching a certain pattern

--- mediagenerator/generators/copyfiles.py
+++ mediagenerator/generators/copyfiles.py
@@ -4,9 +4,28 @@ from mediagenerator.base import Generator
 from mediagenerator.utils import get_media_dirs, find_file
 from mimetypes import guess_type
 import os
+import re
 
 COPY_MEDIA_FILETYPES = getattr(settings, 'COPY_MEDIA_FILETYPES',
     ('gif', 'jpg', 'jpeg', 'png', 'svg', 'ico', 'swf'))
+
+def _prepare_patterns(patterns):
+    if isinstance(patterns, basestring):
+        patterns = (patterns,)
+    if not patterns:
+        return None
+    # First validate each pattern individually
+    for pattern in patterns:
+        try:
+            re.compile(pattern, re.U)
+        except re.error:
+            raise ValueError("""Pattern "%s" can't be compiled """
+                             "in IGNORE_MEDIA_COPY_PATTERNS")
+    # Now return a combined pattern
+    return re.compile('|'.join(patterns), re.U)
+
+IGNORE_PATTERN = _prepare_patterns(getattr(settings,
+   'IGNORE_MEDIA_COPY_PATTERNS', ()))
 
 class CopyFiles(Generator):
     def get_dev_output(self, name):
@@ -32,7 +51,8 @@ class CopyFiles(Generator):
         for root_path, dirs, files in os.walk(root):
             for file in files:
                 ext = os.path.splitext(file)[1].lstrip('.')
-                if ext in COPY_MEDIA_FILETYPES:
-                    path = os.path.join(root_path, file)
-                    media_path = path[len(root)+1:].replace(os.sep, '/')
+                path = os.path.join(root_path, file)
+                media_path = path[len(root)+1:].replace(os.sep, '/')
+                if ext in COPY_MEDIA_FILETYPES and \
+                        not (IGNORE_PATTERN and IGNORE_PATTERN.match(media_path)):
                     media_files[media_path] = path
