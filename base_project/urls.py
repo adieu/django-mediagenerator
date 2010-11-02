dcd987e8e46d4f9a1ccf51b83b59597172dd534a
Waldemar Kornewald <wkornewald>
2010-11-02 10:49:12 +0100

    fixed base_project

--- base_project/urls.py
+++ base_project/urls.py
@@ -4,8 +4,3 @@ from django.conf import settings
 urlpatterns = patterns('',
     (r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'home.html'}),
 )
-
-if settings.MEDIA_DEV_MODE:
-    # Generate media on-the-fly
-    from mediagenerator.urls import urlpatterns as mediaurls
-    urlpatterns += mediaurls
