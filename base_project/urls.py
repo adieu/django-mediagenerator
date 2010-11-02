7236b3d08bb18874fbf6f8aee82715d59c41f343
Waldemar Kornewald <wkornewald>
2010-11-02 13:40:55 +0100

    removed unnecessary import

--- base_project/urls.py
+++ base_project/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls.defaults import *
-from django.conf import settings
 
 urlpatterns = patterns('',
     (r'^$', 'django.views.generic.simple.direct_to_template', {'template': 'home.html'}),
