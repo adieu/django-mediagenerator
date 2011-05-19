22ffd169cbc36d8ab7fc4e85212f7516b7edb24a
Waldemar Kornewald <wkornewald>
2011-05-19 19:14:14 +0200

    bumped version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except:
     pass
 
 setup(name='django-mediagenerator',
-      version='1.10.1',
+      version='1.10.2',
       packages=find_packages(exclude=('tests', 'tests.*',
                                       'base_project', 'base_project.*')),
       package_data={'mediagenerator.filters': ['pyjslibs/*.py', '*.rb'],
