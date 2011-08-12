8c9932da0083c430adac4ed30ddd4c04d86a1aa9
Waldemar Kornewald <wkornewald>
2011-08-12 12:19:33 +0200

    added default mimetype

--- mediagenerator/middleware.py
+++ mediagenerator/middleware.py
@@ -43,6 +43,8 @@ class MediaMiddleware(object):
         except KeyError:
             raise Http404('No such media file "%s"' % filename)
         content, mimetype = backend.get_dev_output(filename)
+        if not mimetype:
+            mimetype = 'application/octet-stream'
         if isinstance(content, unicode):
             content = content.encode('utf-8')
         if mimetype.startswith('text/') or mimetype in TEXT_MIME_TYPES:
